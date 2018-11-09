a17b0ff58eabbfcc7601853dd89742a7829bc066
whossname <tyson.buzza@gmail.com>
2018-11-09 20:17:27 +0800

    test logout

--- test/azure_ad_openid_test.exs
+++ test/azure_ad_openid_test.exs
@@ -1,3 +1,9 @@
 defmodule AzureAdOpenIdTest do
   use ExUnit.Case
+
+  test "build logout url" do
+    actual = AzureAdOpenId.logout_url("tenant", "client_id")
+    expected = "https://login.microsoftonline.com/tenant/oauth2/logout?client_id=client_id"
+    assert actual == expected
+  end
 end
