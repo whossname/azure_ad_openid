df96890e0ace2b825b4d32de32acc89f8f315011
whossname <tyson.buzza@gmail.com>
2019-05-09 09:44:31 +0800

    separate public_key creating from the callback module

--- lib/azure_ad_openid/callback.ex
+++ lib/azure_ad_openid/callback.ex
@@ -6,19 +6,14 @@ defmodule AzureADOpenId.Callback do
   token and then the returned code is used to verify the claims on the token.
   """
 
-  alias JsonWebToken.Algorithm.RsaUtil
   alias AzureADOpenId.VerifyClaims
   alias AzureADOpenId.Enforce
+  alias AzureADOpenId.PublicKey
 
   def process_callback!(id_token, code, config) do
-    public_key =
-      id_token
-      |> get_x5t_from_token!
-      |> get_public_key
-
     opts = %{
       alg: "RS256",
-      key: public_key
+      key: PublicKey.from_token(id_token)
     }
 
     id_token
@@ -28,83 +23,13 @@ defmodule AzureADOpenId.Callback do
   end
 
   def validate_access_token(access_token) do
-    public_key =
-      access_token
-      |> get_x5t_from_token!
-      |> get_public_key()
-
     opts = %{
       alg: "RS256",
-      key: public_key
+      key: PublicKey.from_token(access_token)
     }
 
     access_token
     |> JsonWebToken.verify(opts)
     |> Enforce.ok!("JWT verification failed")
-  end
-
-  defp get_x5t_from_token!(id_token) do
-    error = "Failed to get x5t from token - invalid response"
-
-    id_token
-    # get token header
-    |> String.split(".")
-    |> List.first
-    # decode
-    |> Base.url_decode64(padding: false)
-    |> Enforce.ok!(error)
-    |> Jason.decode!
-    # get x5t
-    |> Map.get("x5t")
-  end
-
-  defp get_public_key(x5t) do
-    jwks_uri!()
-    |> get_discovery_keys!(x5t)
-    |> get_pubilc_key_from_cert
-    |> RsaUtil.public_key
-  end
-
-  defp jwks_uri! do
-    "https://login.microsoftonline.com/common/.well-known/openid-configuration"
-    |> http_request!
-    |> Jason.decode!
-    |> Map.get("jwks_uri")
-  end
-
-  defp get_discovery_keys!(url, x5t)do
-    url
-    |> http_request!
-    |> Jason.decode!
-    |> Map.get("keys")
-    |> Enum.filter(fn(key) -> key["x5t"] === x5t end)
-    |> List.first
-    |> Map.get("x5c")
-  end
-
-  # always use the first x5t value
-  defp get_pubilc_key_from_cert([cert | _]) do
-    spki =
-      "-----BEGIN CERTIFICATE-----\n#{cert}\n-----END CERTIFICATE-----\n"
-      |> :public_key.pem_decode
-      |> hd
-      |> :public_key.pem_entry_decode
-      |> elem(1)
-      |> elem(7)
-
-    :public_key.pem_entry_encode(:SubjectPublicKeyInfo, spki)
-    |> List.wrap
-    |> :public_key.pem_encode
-  end
-
-  defp http_request!(url) do
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        body
-      {:ok, %HTTPoison.Response{status_code: code}} ->
-        raise "HTTP request error. Status Code: #{code} URL: #{url}"
-      {:error, error} ->
-        raise error
-    end
   end
 end
---
+++ lib/azure_ad_openid/public_key.ex
@@ -0,0 +1,77 @@
+defmodule AzureADOpenId.PublicKey do
+  alias JsonWebToken.Algorithm.RsaUtil
+  alias AzureADOpenId.Enforce
+
+  def from_token(token) do
+    token
+    |> get_x5t_from_token!()
+    |> get_public_key()
+  end
+
+  defp get_x5t_from_token!(id_token) do
+    error = "Failed to get x5t from token - invalid response"
+
+    id_token
+    # get token header
+    |> String.split(".")
+    |> List.first()
+    # decode
+    |> Base.url_decode64(padding: false)
+    |> Enforce.ok!(error)
+    |> Jason.decode!()
+    # get x5t
+    |> Map.get("x5t")
+  end
+
+  defp get_public_key(x5t) do
+    jwks_uri!()
+    |> get_discovery_keys!(x5t)
+    |> get_pubilc_key_from_cert
+    |> RsaUtil.public_key()
+  end
+
+  defp jwks_uri! do
+    "https://login.microsoftonline.com/common/.well-known/openid-configuration"
+    |> http_request!
+    |> Jason.decode!()
+    |> Map.get("jwks_uri")
+  end
+
+  defp get_discovery_keys!(url, x5t) do
+    url
+    |> http_request!
+    |> Jason.decode!()
+    |> Map.get("keys")
+    |> Enum.filter(fn key -> key["x5t"] === x5t end)
+    |> List.first()
+    |> Map.get("x5c")
+  end
+
+  # always use the first x5t value
+  defp get_pubilc_key_from_cert([cert | _]) do
+    spki =
+      "-----BEGIN CERTIFICATE-----\n#{cert}\n-----END CERTIFICATE-----\n"
+      |> :public_key.pem_decode()
+      |> hd
+      |> :public_key.pem_entry_decode()
+      |> elem(1)
+      |> elem(7)
+
+    :public_key.pem_entry_encode(:SubjectPublicKeyInfo, spki)
+    |> List.wrap()
+    |> :public_key.pem_encode()
+  end
+
+  defp http_request!(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        body
+
+      {:ok, %HTTPoison.Response{status_code: code}} ->
+        raise "HTTP request error. Status Code: #{code} URL: #{url}"
+
+      {:error, error} ->
+        raise error
+    end
+  end
+end
