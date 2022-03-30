82dbd9d9e30a87bb0175a94febc9e7c16abc33c0
tyson <tyson.buzza@gmail.com>
2022-03-30 16:05:21 +0800

    update version

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule AzureAdOpenid.MixProject do
   use Mix.Project
-  @version "0.3.2"
+  @version "0.3.3"
   @url "https://github.com/whossname/azure_ad_openid"
   @maintainers ["Tyson Buzza"]
 
